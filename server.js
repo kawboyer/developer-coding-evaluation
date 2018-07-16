c0fba7e1290c4fdf68b8ae53205c68492dc6e459
Klara Boyer <kawboyer@gmail.com>
2018-07-16 17:17:03 -0400

    deployment to heroku

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const port = process.env.PORT || 5000;
 if (process.env.NODE_ENV === 'production') {
 
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/public')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
   // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 app.listen(port, () => console.log(`Listening on port ${port}`));
