951e121c886187f57f1b51872e7baf19a96e5eac
Klara Boyer <kawboyer@gmail.com>
2018-07-27 13:01:28 -0400

    grid css for clients component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import Header from './components/Header.js';
 import Services from './components/Services.js';
-// import Clients from './components/Clients.js';
+import Clients from './components/Clients.js';
 // import Gojo from './components/Gojo.js';
 // import FifthThird from './components/FifthThird.js';
 // import Chhn from './components/Chhn.js';
 // import Connect from './components/Connect.js';
-import Footer from './components/Footer.js';
+// import Footer from './components/Footer.js';
 import './App.css';
 
 
@@ -20,12 +20,12 @@ class App extends Component {
       <div className="App">
         <Header />
         <Services />
-        {/* <Clients /> */}
-        {/* <Gojo /> */}
+        <Clients />
+        <Gojo />
         {/* <FifthThird /> */}
         {/* <Chhn /> */}
         {/* <Connect /> */}
-        <Footer />
+        {/* <Footer /> */}
       </div>
     );
   }
