47523c2f7a39a36c46d2b62cec3778b82b252f7f
Klara Boyer <kawboyer@gmail.com>
2018-08-01 17:27:30 -0400

    adjustments and cleanup of code

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,8 +7,8 @@ const Header = () => (
   <header>
     <div className="grid-container-header">
       
-      <img id="background-image-header" src={hero} alt="" /> 
-      <img id="logo-cardinal" src={logo} alt="Company Logo" />
+      <img id="image-header" src={hero} alt="" /> 
+      <img id="logo-header" src={logo} alt="Company Logo" />
       <h1 className="title-header"><span>Enjoy the proven success of our experience.</span></h1>
 
     </div>   
