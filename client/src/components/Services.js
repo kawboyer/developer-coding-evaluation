477e50008ef20ef9b0e42f539f6cd77d3a0a4834
Klara Boyer <kawboyer@gmail.com>
2018-08-01 19:00:46 -0400

    adjustments and cleanup code services component

--- client/src/components/Services.js
+++ client/src/components/Services.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import bi from '../assets/icons/icon-bi.svg';
 import appdev from '../assets/icons/icon-appdev.svg';
 import ba from '../assets/icons/icon-ba.svg';
-// import '../css/Grid.css';
+import bi from '../assets/icons/icon-bi.svg';
 import '../css/Services.css';
 
 const Services = () => (
@@ -12,26 +11,27 @@ const Services = () => (
       <div id="section-1">
         <img id="img-bi" src={bi} alt="Logo for Business Intelligence"/>
         <div>
-          <h3 id="section-1-title">Business Intelligence</h3>
-          <p className="section">We use technologies to turn data into knowledge that can help you manage and monitor your business, from anywhere.</p>
+          <p className="subtitle">Business Intelligence</p>
+          <p className="body">We use technologies to turn data into knowledge that can help you manage and monitor your business, from anywhere.</p>
         </div>
       </div>
 
       <div id="section-2">
         <img id="img-appdev" src={appdev} alt="Logo for App Development"/>
         <div>
-          <h3 id="section-2-title">App Development</h3>
-          <p className="section">Our mobile specialists work to create rich, easy-to-use consumer or enterprise experiences for iOS, Android.</p>
+          <p className="subtitle">App Development</p>
+          <p className="body body-section-2">Our mobile specialists work to create rich, easy-to-use consumer or enterprise experiences for iOS, Android.</p>
         </div>
       </div>
 
       <div id="section-3">
         <img id="img-ba" src={ba} alt="Logo for Business Analysis"/>
         <div>
-          <h3 id="section-3-title">Business Analysis</h3>
-          <p className="section">Great strategy begins with listening. We learn about businesses from the inside out, and then propose improvements.</p>
+          <p className="subtitle">Business Analysis</p>
+          <p className="body">Great strategy begins with listening. We learn about businesses from the inside out, and then propose improvements.</p>
         </div>
       </div>
+
     </div>
   </section>
 )
