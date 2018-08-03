5d53a00629a1f4f5889116aee25a003931d2dc25
Klara Boyer <kawboyer@gmail.com>
2018-08-02 20:25:21 -0400

    adjustment and cleanup of code gojo component

--- client/src/components/Gojo.js
+++ client/src/components/Gojo.js
@@ -6,10 +6,11 @@ import '../css/Gojo.css';
 const Gojo = () => (
   <section> 
     <div className="grid-container-gojo">
-      <div id="color-background-gojo"></div>
+      <div id="background-gojo"></div>
       <img id="image-bg-gojo" src={imageBg} alt="Gojo Background" width="100%" />
       <img id="image-gojo" src={image} alt="Gojo"/>
-      <div id="color-background-gojo2"></div>
+      <div id="background-gojo2"></div>
+      <div id="background-gojo3"></div>
       <h1 className="title-gojo">
         Gojo
       </h1>
@@ -19,7 +20,7 @@ const Gojo = () => (
       <p className="body-gojo">
         Reducing the spread of hospital infections with the Internet of Things.
       </p>
-      <button id="button-gojo">Learn More</button>
+      <button className="button-gojo">Learn More</button>
     </div>
   </section>
 );
