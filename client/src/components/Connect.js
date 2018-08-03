3507dbf488d1d14b236a839971c795367bfeddb5
Klara Boyer <kawboyer@gmail.com>
2018-08-03 11:58:40 -0400

    adjustments and cleanup of code mobile connect component

--- client/src/components/Connect.js
+++ client/src/components/Connect.js
@@ -15,18 +15,18 @@ const Connect = () => (
             <input type="text" placeholder="Name" id="input-name" />
             <input type="text" placeholder="Email" id="input-email" />
             <input type="text" placeholder="Message" id="input-message" />
-            <button type="submit" value="Submit" id="button-submit-connect">Submit</button>
+            <button type="submit" value="Submit" className="button-submit-connect">Submit</button>
           </form>
 
           {/* <hr> */}
 
-          <div className="contact">
+          <div className="content-contact">
             <p className="subtitle-connect">For inquieres, please contact:</p>
             <p>Tracy Thompson</p>
             <p>tthompson@cardinalsolutions.com</p>
             <p id="contact-phone">704.936.4444</p>
 
-            <p className="subtitle-connect">Come by our office</p>
+            <p className="subtitle-connect-2">Come by our office</p>
             <p>222 S. Church St. Suite 500,</p>
             <p>Charlotte, NC 28202</p>
             <img id="map" src={map} alt="Map showing office location" />
